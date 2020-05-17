3b2a0caec5f80dbe26d93fc95ad56a0e51fb893e
Lauren Cunningham <lauren.busbee@gmail.com>
2020-05-17 19:41:57 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,9 @@
 def square_array(array)
-  nums = [1, 2, 3]
   new = []
-  
-  nums.each do |num|
-     new.push #{num}**2
-end
 
-square_array(nums)
-square_array(new)
+  array.each do |int| 
+    new << int ** 2
+  end
+
+  return new
+end
